d5d3803b75b74b5abe3d4d977a1830c59f534d6c
murilodcosta <murilodiasdacosta@gmail.com>
2025-11-14 20:41:25 -0300

    Squashed 'computer-science/06-semester/algorithms-and-prog-lang-paradigms/exercises/grafos-dijkstra/' content from commit b82aa50
    git-subtree-dir: computer-science/06-semester/algorithms-and-prog-lang-paradigms/exercises/grafos-dijkstra
    git-subtree-split: b82aa50ae393386821eab9ea737a5c5444836705

---
+++ src/main/java/com/example/routing/Main.java
@@ -0,0 +1,25 @@
+package com.example.routing;
+
+import com.example.routing.service.RotasService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(RotasService rotasService) {
+        return args -> {
+            // Alterado para chamar o novo método do exercício da aula - Metrô
+            rotasService.exercicioAula13_Metro();
+
+            // rotasService.executarTodosProblemas(); // Método original
+        };
+    }
+}
