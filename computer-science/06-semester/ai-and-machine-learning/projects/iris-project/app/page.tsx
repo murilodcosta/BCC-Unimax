e2c461d7d805bb2eba16a271d9c0d58692679663
murilodcosta <murilodiasdacosta@gmail.com>
2025-11-14 09:50:24 -0300

    feat: Main page and Navbar initial adjustments

--- computer-science/06-semester/ai-and-machine-learning/projects/iris-project/app/page.tsx
+++ computer-science/06-semester/ai-and-machine-learning/projects/iris-project/app/page.tsx
@@ -26,29 +26,18 @@ export default async function Home() {
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <a
-          href="https://twitter.com/steventey/status/1613928948915920896"
-          target="_blank"
-          rel="noreferrer"
-          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
-          <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing Precedent
-          </p>
-        </a>
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          Building blocks for your Next project
+          O futuro do diagnóstico na palma da sua mão
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
-          An opinionated collection of components, hooks, and utilities for your
-          Next.js project.
+          Seu assistente em diagnósticos por imagem. Integre nossa IA para uma triagem eficiente e identificação de padrões, 
+          liberando seu tempo para decisões clínicas complexas.
         </p>
         <div
           className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
@@ -60,7 +49,7 @@ export default async function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg
+            {/* <svg
               className="h-4 w-4 group-hover:text-black"
               viewBox="0 0 24 24"
               fill="currentColor"
@@ -73,8 +62,8 @@ export default async function Home() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
               />
-            </svg>
-            <p>Deploy to Vercel</p>
+            </svg> */}
+            <p>Upload</p>
           </a>
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
--- computer-science/06-semester/ai-and-machine-learning/projects/iris-project/components/layout/navbar.tsx
+++ computer-science/06-semester/ai-and-machine-learning/projects/iris-project/components/layout/navbar.tsx
@@ -27,7 +27,7 @@ export default function NavBar() {
               height="30"
               className="mr-2 rounded-sm"
             ></Image>
-            <p>Precedent</p>
+            <p>Iris</p>
           </Link>
           <SignedOut>
             <SignInButton mode="modal">
